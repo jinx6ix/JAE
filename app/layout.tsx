f33c862e57138a49dda319d2a35aca693cf88cbb
jinx6ix <irayaian15@gmail.com>
2025-09-09 16:58:40 +0300

    added gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -132,7 +132,7 @@ export default function RootLayout({
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-XXXXXX');
+              })(window,document,'script','dataLayer','GTM-PJJQMMX7');
             `,
           }}
         />
@@ -142,7 +142,7 @@ export default function RootLayout({
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXX"
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJJQMMX7"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>
             `,
           }}
